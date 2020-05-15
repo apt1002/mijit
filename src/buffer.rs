33c39042bf735af62a8f75c7e761e74464c0da41
Alistair Turnbull <github@minworks.co.uk>
2020-05-15 01:50:48 +0100

    Add field `used` to struct Buffer

--- src/buffer.rs
+++ src/buffer.rs
@@ -5,15 +5,15 @@ use memmap::{MmapMut};
 #[repr(C)]
 pub struct Buffer {
     memory: MmapMut,
+    used: usize,
 }
 
 impl Buffer {
     /** Allocates a Buffer. Returns `None` if not possible. */
     pub fn new(capacity: usize) -> Option<Self> {
         match MmapMut::map_anon(capacity) {
-            Ok(memory) => Some(Buffer {memory}),
+            Ok(memory) => Some(Buffer {memory, used: 0}),
             Err(_) => None
         }
     }
-    
 }
