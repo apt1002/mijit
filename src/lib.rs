2c21548cc9181a4af8ded6df0c7b194bb98e4b70
Reuben Thomas <rrt@sc3d.org>
2020-06-25 16:07:44 +0100

    lib.rs: add a test that assembles and calls some code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod code;
 pub mod control_flow;
+
 pub mod x86_64;
+pub use x86_64::{Assembler};
+
 pub mod history;
 
 pub mod beetle;
@@ -9,3 +12,30 @@ pub mod buffer;
 pub use buffer::{Buffer};
 
 mod c_bindings;
+
+//-----------------------------------------------------------------------------
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn add5() {
+        use std::{mem};
+        use x86_64::*;
+        use Register::*;
+        use BinaryOp::*;
+
+        let mut buffer = Buffer::new(0x1000)
+            .expect("Couldn't allocate");
+        let mut a = Assembler::new(&mut buffer);
+        a.mov(RA, RDI);
+        a.const_op(Add, RA, 5);
+        a.ret();
+        let (_, result) = buffer.execute(|bytes| {
+            let f: extern "C" fn(i32) -> i32 = unsafe {mem::transmute(&bytes[0])};
+            f(42)
+        }).expect("Couldn't change permissions");
+        assert_eq!(result, 42 + 5);
+    }
+}
