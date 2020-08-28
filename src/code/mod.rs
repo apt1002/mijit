93b7a01edc9b00f77a44d3be17ddff95c6f55c91
Alistair Turnbull <apt1002@frog>
2020-08-28 01:06:40 +0100

    code: Incorporate `mod control_flow` into the main source file.
    Also, update and expand the comments.

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -2,19 +2,25 @@
  * Mijit's instruction set. This instruction set is used to define virtual
  * machines, and it is also used to remember what code Mijit has generated.
  *
- * Control flow is restricted to a finite state machine, defined by
- * implementing trait Machine. All the other instructions are branch-free.
+ * A virtual machine's control flow is restricted to a finite state machine,
+ * defined by implementing trait [`Machine`]. All the other instructions are
+ * branch-free. More complex control flow can be achieved by driving the finite
+ * state machine using values loaded from memory.
  *
- * Arithmetic operations are all 32-bit (for now). The upper 32 bits of the
- * destination register will be set to zero.
+ * A virtual machine's storage is restricted to a finite number of global
+ * variables, again defined by implementing trait [`Machine`]. More complex
+ * data structures can be achieved by loading and storing values in memory.
+ *
+ * Arithmetic operations are 32-bit or 64-bit. 32-bit operations set the upper
+ * 32 bits of the destination register to zero.
  *
  * Booleans results are returned as `0` or `-1`.
  */
 
-pub use super::x86_64::{Register as R, Precision};
+use std::fmt::{Debug};
+use std::hash::{Hash};
 
-pub mod control_flow;
-pub use control_flow::{Machine, Alias};
+pub use super::x86_64::{Register as R, Precision};
 
 pub mod clock;
 
@@ -99,4 +105,72 @@ pub enum Action<M, G> {
     Push(R),
     Pop(R),
     Debug(R),
+}
+
+/** Test whether memories overlap. */
+// TODO: Replace with a bitmask.
+// TODO: Actually use this.
+pub trait Alias: Debug + Clone + Hash + Eq {
+    /**
+     * Tests whether an address in `self` and an address in `other` might refer
+     * to the same storage location. More precisely, tests whether the value
+     * read from one of the addresses can be affected by a write to the other.
+     *
+     * The relation:
+     *  - must be symmetric. `m.can_alias(&n)` must equal `n.can_alias(&m)`.
+     *  - must be reflexive if the memory is mutable. If all writes to an
+     *    address in `m` precede all reads from the same address, then
+     *    `m.can_alias(&m)` can be `false`, otherwise it must be `true`.
+     *  - need not be transitive. It is possible that `m.can_alias(&n1)` and
+     *    `m.can_alias(&n2)` but not `n1.can_alias(&n2)`.
+     *
+     * An implementation of `can_alias()` that always returns `true` is
+     * allowed, but it is unhelpful. The purpose of this method is to enable
+     * the following transformations, which are invalid if `m.can_alias(&n)`:
+     *  - Store, load:
+     *     - Before: store(n); load(m)
+     *     - After: load(m); store(n)
+     *  - Load, store:
+     *     - Before: load(m); store(n)
+     *     - After: store(n); load(m)
+     *  - Store, store:
+     *     - Before: store(n); store(m)
+     *     - After: store(m)
+     *
+     * The default implementation returns `*self == *other`.
+     */
+    fn can_alias(&self, other: &Self) -> bool {
+        *self == *other
+    }
+}
+
+pub trait Machine: Debug {
+    /** A state of the finite state machine. */
+    type State: Debug + Clone + Hash + Eq;
+
+    /** A discrete VM storage location, such as a register. */
+    type Global: Debug + Clone + Hash + Eq;
+
+    /** A VM storage location with an address. */
+    type Memory: Alias;
+
+    /**
+     * Defines the transitions of the finite state machine.
+     *  - state - the source State.
+     * Returns a (condition, actions, new_state) for each transition from
+     * `state`:
+     *  - condition - when to use the transition. Mijit selects the first
+     *    transition with a true condition.
+     *  - actions - code to execute when the transition is selected.
+     *  - new_state - the destination State.
+     */
+    fn get_code(&self, state: Self::State) ->
+        Vec<(
+            (TestOp, Precision),
+            Vec<Action<Self::Memory, Self::Global>>,
+            Self::State,
+        )>;
+
+    /** Returns some States from which all others are reachable. */
+    fn initial_states(&self) -> Vec<Self::State>;
 }
