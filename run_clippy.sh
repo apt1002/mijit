75c14bdb0a66b18a6190ca2e8ff97a53221b8653
Alistair Turnbull <github@minworks.co.uk>
2021-04-10 16:42:43 +0100

    clippy: pedantic: Always allow `enum_glob_use`.

--- run_clippy.sh
+++ run_clippy.sh
@@ -19,4 +19,5 @@ cargo clippy --color=always $FIX_OPTIONS -- $PEDANTIC_OPTIONS \
   -A clippy::cast_possible_wrap \
   -A clippy::cast_sign_loss \
   -A clippy::must_use_candidate \
+  -A clippy::enum_glob_use \
   "$@"
