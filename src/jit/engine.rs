16b45ef41180d936b69eb11325ec6cc8bc6c1d8a
Alistair Turnbull <github@minworks.co.uk>
2021-08-09 01:07:30 +0100

    Implement `new_entry()` and `is_defined()`.

--- src/jit/engine.rs
+++ src/jit/engine.rs
@@ -2,31 +2,56 @@ use std::ops::{Index, IndexMut};
 
 use crate::util::{AsUsize};
 use super::{code};
-use super::target::{Label, Word, Pool, Lower, Execute, Target};
-use code::{Global};
+use super::target::{Label, Counter, Word, Pool, Lower, Execute, Target, RESULT};
+use code::{Precision, Global, Switch, Action};
+use Precision::*;
 
 // CaseId.
 array_index! {
-    /** Identifies a specialization of an `Engine`. */
+    /** Identifies a [`Case`] of an [`Engine`]. */
     #[derive(Copy, Clone, Hash, PartialEq, Eq)]
     pub struct CaseId(std::num::NonZeroUsize) {
-        debug_name: "Specialization",
+        debug_name: "CaseId",
         UInt: usize,
     }
 }
+
+/** The part of a [`Case`] which changes when it is specialized. */
+enum Junction {
+    /** Not yet specialized. Count, and retire to another [`Case`]. */
+    Retire {
+        /** The profiling [`Counter`] to increment. */
+        counter: Counter,
+        /** The code to run. */
+        retire_code: Box<[Action]>,
+        /** The [`Case`] to jump to. `None` means the root. */
+        jump: Option<CaseId>,
+    },
+    /** Specialized. Run special code and pick a continuation. */
+    Fetch {
+        /** The code to run. */
+        fetch_code: Box<[Action]>,
+        /** The control-flow decision. */
+        switch: Switch<CaseId>,
+    },
+}
+
+use Junction::*;
 
 /**
  * Represents a basic block ending with some kind of branch.
  * See "doc/engine/structure.md".
  */
 struct Case {
-    fetch_label: Label,
+    label: Label,
+    junction: Junction,
 }
 
 //-----------------------------------------------------------------------------
 
 /** An entry point into the compiled code. */
 pub struct Entry {
+    label: Label,
     case: CaseId,
 }
 
@@ -38,8 +63,8 @@ pub struct Entry {
  */
 struct Internals {
     /**
-     * The specializations in the order they were compiled, excluding the
-     * least specialization. Indexed by [`Specialization`].
+     * The [`Case`]s in the order they were compiled, excluding the root.
+     * Indexed by [`CaseId`].
      */
     cases: Vec<Case>,
 }
@@ -71,6 +96,12 @@ impl IndexMut<CaseId> for Internals {
 pub struct Engine<T: Target> {
     /** The compilation target. */
     _target: T,
+    /** The number of [`Slot`]s at every [`Entry`]'s `Case`. */
+    num_slots: usize,
+    /** Executed on entry. */
+    prologue: Box<[Action]>,
+    /** Executed on exit. */
+    epilogue: Box<[Action]>,
     /** The code compiled so far. */
     lowerer: T::Lowerer,
     /** This nested struct can be borrowed independently of `lowerer`. */
@@ -78,17 +109,100 @@ pub struct Engine<T: Target> {
 }
 
 impl<T: Target> Engine<T> {
-    pub fn new(target: T, num_globals: usize) -> Self {
+    /**
+     * Constructs an `Engine`, initially with no [`Entry`]s.
+     *  - num_globals - the number of `Global`s needed to pass values to and
+     *    from the compiled code.
+     *  - num_slots - the number of `Slot`s that are live at every `Entry`.
+     *    Must be even.
+     *  - prologue - executed on every entry to the compiled code.
+     *  - epilogue - executed on every exit from the compiled code.
+     */
+    pub fn new(target: T, num_globals: usize, num_slots: usize, prologue: Box<[Action]>, epilogue: Box<[Action]>) -> Self {
+        assert_eq!(num_slots & 1, 0);
         let pool = Pool::new(num_globals);
         let lowerer = target.lowerer(pool);
         let internals = Internals {
             cases: Vec::new(),
         };
-        Engine {_target: target, lowerer, internals}
+        Engine {_target: target, num_slots, prologue, epilogue, lowerer, internals}
     }
 
+    /** Borrows the value of variable `global`. */
     pub fn global_mut(&mut self, global: Global) -> &mut Word {
         &mut self.lowerer.pool_mut()[global]
+    }
+
+    /** Construct a fresh [`Case`] which retires to `jump`. */
+    fn new_case(&mut self, retire_code: Box<[Action]>, jump: Option<CaseId>) -> CaseId {
+        let lo = &mut self.lowerer;
+        // Compile the mutable jump.
+        let mut label = Label::new(None);
+        lo.jump(&mut label);
+        lo.define(&mut label);
+        // Create a `Counter` and compile the code to increment it.
+        let counter = lo.pool_mut().new_counter();
+        lo.count(counter);
+        // Compile `retire_code`.
+        for &action in retire_code.iter() {
+            lo.action(action);
+        }
+        // Compile the jump to `jump`.
+        if let Some(jump) = jump {
+            // Jump to a non-root `Case`.
+            lo.jump(&mut self.internals[jump].label);
+        } else {
+            // Jump to the root.
+            lo.epilogue()
+        }
+        // Record details in a `Case` and return its `CaseId`.
+        let id = CaseId::new(self.internals.cases.len()).unwrap();
+        self.internals.cases.push(Case {
+            label,
+            junction: Retire {counter, retire_code, jump}
+        });
+        id
+    }
+
+    /**
+     * Construct an [`Entry`] to this `Engine`. Initially, the code at the
+     * `Entry` will immediately return `exit_value`. To change this behaviour,
+     * use `define()`.
+     */
+    pub fn new_entry(&mut self, exit_value: i64) -> Entry {
+        assert!(exit_value >= 0);
+        let lo = &mut self.lowerer;
+        // Compile the prologue.
+        let label = lo.here();
+        lo.prologue();
+        for _ in 0..(self.num_slots >> 1) {
+            lo.action(Action::Push(None, None));
+        }
+        for &action in self.prologue.iter() {
+            lo.action(action);
+        }
+        // Compile the epilogue.
+        let mut retire_code = Vec::new();
+        retire_code.extend(self.epilogue.iter().cloned());
+        retire_code.push(Action::DropMany(self.num_slots >> 1));
+        retire_code.push(Action::Constant(P64, RESULT, exit_value));
+        let case = self.new_case(retire_code.into(), None);
+        // Return.
+        Entry {label, case}
+    }
+
+    /** Tests whether [`define(entry, ...)`] has been called. */
+    pub fn is_defined(&self, entry: Entry) -> bool {
+        matches!(self.internals[entry.case].junction, Fetch {..})
+    }
+
+    /**
+     * Replace the code at `Entry` such that it executes `actions` and then
+     * jumps to the `Entry` selected by `switch`. Each `Entry` may only be
+     * defined once.
+     */
+    pub fn define(&mut self, entry: Entry, actions: Box<[Action]>, switch: Switch<Entry>) {
+        assert!(!self.is_defined(entry));
     }
 
     /**
@@ -99,9 +213,8 @@ impl<T: Target> Engine<T> {
      * This will crash if the code is compiled for the wrong [`Target`] or if
      * the code returned by the [`Machine`] is invalid.
      */
-    pub unsafe fn execute(mut self, entry: Entry) -> std::io::Result<(Self, Word)> {
-        let label = &self.internals[entry.case].fetch_label;
-        let (lowerer, ret) = self.lowerer.execute(label, |f, pool| {
+    pub unsafe fn run(mut self, entry: Entry) -> std::io::Result<(Self, Word)> {
+        let (lowerer, ret) = self.lowerer.execute(&entry.label, |f, pool| {
             let pool = pool.as_mut().as_mut_ptr();
             // Here is a good place to set a gdb breakpoint.
             f(pool)
--- src/jit/mod.rs
+++ src/jit/mod.rs
@@ -1,6 +1,8 @@
 use super::{code, target};
 
 mod engine;
+// TODO: Make private. Having these public reduces "unused" warnings.
+pub use engine::{Engine, Entry};
 
 //mod machine;
 //pub use machine::{Jit};
