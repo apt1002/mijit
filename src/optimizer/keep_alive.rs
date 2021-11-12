9f455b025f4a42fe028851c4e15175724e6891b6
Alistair Turnbull <github@minworks.co.uk>
2021-11-12 10:32:37 +0000

    Reimplement `keep_alive_sets()` using a LIFO queue.
    This topologically sorts the visited Nodes, as needed by a Schedule.

--- src/optimizer/keep_alive.rs
+++ src/optimizer/keep_alive.rs
@@ -1,66 +1,89 @@
 use std::collections::{HashMap, HashSet};
 
-use crate::util::{ArrayMap, Fifo};
+use crate::util::{ArrayMap};
 use super::{Dataflow, Node, Out, CFT};
+
+//-----------------------------------------------------------------------------
+
+/** The state of a LIFO flood fill through a [`Dataflow`] graph. */
+struct Flood<'a> {
+    /** The graph to flood fill. */
+    dataflow: &'a Dataflow,
+    /** Records which [`Node`]s have been visited. */
+    marks: &'a mut ArrayMap<Node, usize>,
+    /** The value to store in `marks` when we visit a [`Node`]. */
+    temperature: usize,
+    /** Accumulates the live inputs. */
+    inputs: &'a mut HashSet<Out>,
+    /** Accumulates the visited [`Node`]s, topologically sorted. */
+    nodes: Vec<Node>,
+}
+
+impl <'a> Flood<'a> {
+    pub fn new(
+        dataflow: &'a Dataflow,
+        marks: &'a mut ArrayMap<Node, usize>,
+        temperature: usize,
+        inputs: &'a mut HashSet<Out>,
+    ) -> Self {
+        Self {dataflow, marks, temperature, inputs, nodes: Vec::new()}
+    }
+
+    /**
+     * Flood the unmarked [`Node`]s on which `node` depends.
+     *
+     * The fill follows `dataflow` dependencies (both values and side-effects)
+     * backwards in time. Visited [`Node`]s are marked with `temperature`,
+     * and are added to `nodes` after all of their dependencies. The fill stops
+     * at [`Node`]s that are marked with a non-zero value; the [`Out`]s by
+     * which they are reached are added to `inputs`.
+     */
+    pub fn flood(&mut self, node: Node) {
+        assert_eq!(self.marks[node], 0);
+        self.marks[node] = self.temperature;
+        // TODO: Sort `Node`s by latency or breadth or something.
+        for &node in self.dataflow.deps(node) {
+            if self.marks[node] == 0 {
+                self.flood(node);
+            }
+        }
+        for &out in self.dataflow.ins(node) {
+            let (node, _) = self.dataflow.out(out);
+            if self.marks[node] == 0 {
+                self.flood(node);
+            } else if self.marks[node] < self.temperature {
+                self.inputs.insert(out);
+            } else {
+                assert_eq!(self.marks[node], self.temperature);
+            }
+        }
+        self.nodes.push(node);
+    }
+}
 
 //-----------------------------------------------------------------------------
 
 struct KeepAlive<'a> {
     dataflow: &'a Dataflow,
-    mark: ArrayMap<Node, usize>,
+    marks: ArrayMap<Node, usize>,
     keep_alives: HashMap<Node, Box<[Out]>>,
 }
 
 impl<'a> KeepAlive<'a> {
     fn new(dataflow: &'a Dataflow) -> Self {
-        let mut mark = dataflow.node_map();
-        mark[dataflow.entry_node()] = 1;
+        let mut marks = dataflow.node_map();
+        marks[dataflow.entry_node()] = 1;
         let keep_alives = HashMap::new();
-        KeepAlive {dataflow, mark, keep_alives}
-    }
-
-    /**
-     * Finds the live inputs and live nodes of the unmarked portion of
-     * `dataflow` on which `exit` depends. Marks all the live nodes with
-     * `temperature`. Adds the live inputs to `inputs`.
-     */
-    fn flood(&mut self, exit: Node, temperature: usize, inputs: &mut HashSet<Out>)
-    -> Vec<Node> {
-        let mut fifo = Fifo::new();
-        // Enqueue `exit`.
-        assert_eq!(self.mark[exit], 0);
-        self.mark[exit] = temperature;
-        fifo.enqueue(exit);
-        // Breadth-first flood fill.
-        while let Some(&node) = fifo.dequeue() {
-            for &node in self.dataflow.deps(node) {
-                if self.mark[node] == 0 {
-                    self.mark[node] = temperature;
-                    fifo.enqueue(node);
-                }
-            }
-            for &out in self.dataflow.ins(node) {
-                let (node, _) = self.dataflow.out(out);
-                if self.mark[node] == 0 {
-                    self.mark[node] = temperature;
-                    fifo.enqueue(node);
-                } else if self.mark[node] < temperature {
-                    inputs.insert(out);
-                } else {
-                    assert_eq!(self.mark[node], temperature);
-                }
-            }
-        }
-        fifo.finish()
+        KeepAlive {dataflow, marks, keep_alives}
     }
 
     /**
      * Add to `self.keep_alives` the keep-alive set for every [`Op::Guard`] in
      * `cft`. Add to `inputs` the live inputs of `cft`.
      *
-     * On entry and on exit, `mark[node]` must be in `1..temperature` if `node`
-     * is on the hotter path from which `cft` diverges, and `0` otherwise.
-     * `mark[entry_node]` must be `1`.
+     * On entry and on exit, `marks[node]` must be in `1..temperature` if
+     * `node` is on the hotter path from which `cft` diverges, and `0`
+     * otherwise. `marks[entry_node]` must be `1`.
      *
      * - temperature - the number of cold branches needed to reach `cft` + 2.
      *   (`0` is used for unmarked nodes, and `1` for the entry node).
@@ -68,7 +91,9 @@ impl<'a> KeepAlive<'a> {
     fn walk(&mut self, cft: &'a CFT, inputs: &mut HashSet<Out>, temperature: usize) {
         let (hot_colds, exit) = cft.hot_path();
         // Mark everything that `exit` depends on.
-        let nodes = self.flood(exit, temperature, inputs);
+        let mut flood = Flood::new(&self.dataflow, &mut self.marks, temperature, inputs);
+        flood.flood(exit);
+        let nodes = flood.nodes;
         // For each guard we passed...
         for hot_cold in hot_colds {
             // Recurse to find all the inputs of any cold path.
@@ -80,8 +105,8 @@ impl<'a> KeepAlive<'a> {
             // Add them to our own inputs if necessary.
             for &out in &*keep_alives {
                 let (node, _) = self.dataflow.out(out);
-                assert_ne!(self.mark[node], 0);
-                if self.mark[node] < temperature {
+                assert_ne!(self.marks[node], 0);
+                if self.marks[node] < temperature {
                     // Hotter than us.
                     inputs.insert(out);
                 }
@@ -91,8 +116,8 @@ impl<'a> KeepAlive<'a> {
         }
         // Unark everything that we marked.
         for node in nodes {
-            assert_eq!(self.mark[node], temperature);
-            self.mark[node] = 0;
+            assert_eq!(self.marks[node], temperature);
+            self.marks[node] = 0;
         }
     }
 }
