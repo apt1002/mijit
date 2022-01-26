1acc0f9579119973a617244547555a88085893a0
Alistair Turnbull <github@minworks.co.uk>
2022-01-26 16:17:48 +0000

    Rewrite factorial test for Jit2 API.

--- src/jit/factorial.rs
+++ src/jit/factorial.rs
@@ -2,14 +2,10 @@ use super::code::*;
 use Action::*;
 use BinaryOp::*;
 use Precision::*;
+use super::target::{Target, Word};
+use super::{EntryId, Jit2};
 
 const R0: Register = REGISTERS[0];
-
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
-pub enum State {Start, Loop}
-
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
-pub enum Trap {Halt}
 
 pub mod reg {
     use super::{Global, Variable};
@@ -17,36 +13,37 @@ pub mod reg {
     pub const RESULT: Variable = Variable::Global(Global(1));
 }
 
-#[derive(Debug)]
-pub struct Machine;
+/** An example that can be used as a test fixture. */
+pub struct Factorial<T: Target> {
+    /** The state of the JIT compiler. */
+    pub jit: Jit2<T>,
+    /** The entry point. */
+    pub start: EntryId,
+}
 
-impl super::code::Machine for Machine {
-    type State = State;
-    type Trap = Trap;
+const START: i64 = 0;
+const LOOP: i64 = 1;
+const HALT: i64 = 2;
 
-    fn num_globals(&self) -> usize { 2 } // reg::N and reg::RESULT
-
-    fn marshal(&self, _state: Self::State) -> Marshal {
-        Marshal {
-            prologue: Box::new([]),
-            epilogue: Box::new([]),
-        }
-    }
-
-    fn code(&self, state: Self::State) -> Switch<Case<Result<Self::State, Self::Trap>>> {
-        match state {
-            State::Start => Switch::always(
-                Case {
-                    actions: vec![
-                        Constant(P32, R0, 1),
-                        Move(reg::RESULT, R0.into()),
-                    ],
-                    new_state: Ok(State::Loop),
-                },
-            ),
-            State::Loop => Switch::if_(
+impl<T: Target> Factorial<T> {
+    pub fn new(target: T) -> Factorial<T> {
+        let mut jit = Jit2::new(target, 2);
+        let marshal = Marshal {prologue: Box::new([]), epilogue: Box::new([])};
+        let start = jit.new_entry(&marshal, START);
+        let loop_ = jit.new_entry(&marshal, LOOP);
+        let halt = jit.new_entry(&marshal, HALT);
+        jit.define(start, &EBB {
+            actions: vec![
+                Constant(P32, R0, 1),
+                Move(reg::RESULT, R0.into()),
+            ],
+            ending: Ending::Leaf(loop_),
+        });
+        jit.define(loop_, &EBB {
+            actions: vec![],
+            ending: Ending::Switch(Switch::if_(
                 reg::N,
-                Case {
+                EBB {
                     actions: vec![
                         Binary(Mul, P32, R0, reg::RESULT, reg::N),
                         Move(reg::RESULT, R0.into()),
@@ -54,17 +51,25 @@ impl super::code::Machine for Machine {
                         Binary(Sub, P32, R0, reg::N, R0.into()),
                         Move(reg::N, R0.into()),
                     ],
-                    new_state: Ok(State::Loop),
+                    ending: Ending::Leaf(loop_),
                 },
-                Case {
+                EBB {
                     actions: vec![],
-                    new_state: Err(Trap::Halt),
+                    ending: Ending::Leaf(halt),
                 },
-            ),
-        }
+            )),
+        });
+        Factorial {jit, start}
     }
-
-    fn initial_states(&self) -> Vec<Self::State> {
-        vec![State::Start]
+    
+    pub fn run(mut self, n: u64) -> std::io::Result<(Self, u64)> {
+        let n_global = Global::try_from(reg::N).unwrap();
+        let result_global = Global::try_from(reg::RESULT).unwrap();
+        *self.jit.global_mut(n_global) = Word {u: n};
+        let (jit, exit_value) = unsafe {self.jit.run(self.start)}?;
+        assert_eq!(exit_value, Word {s: HALT});
+        self.jit = jit;
+        let result = *self.jit.global_mut(result_global);
+        Ok((self, unsafe {result.u}))
     }
 }
