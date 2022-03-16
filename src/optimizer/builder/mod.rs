3c48885ee82f835f9aae2388f75dbdf5fd379f1e
Alistair Turnbull <github@minworks.co.uk>
2022-03-16 17:40:55 +0000

    Fix: introduce `guard_failure` callback argument to `Builder.walk()`.
    The problem occurs when the hot path contains two guards and the
    optimizer decides to reorder them. In this case, the guard that is
    delayed gets replicated on the cold paths of the guard that is advanced.
    Its GuardFailure must therefore be provided to those cold paths.
    
    This fix is necessary, but not sufficient. There is still a bug to be found.

--- src/optimizer/builder/mod.rs
+++ src/optimizer/builder/mod.rs
@@ -9,7 +9,7 @@ mod flood;
 use flood::{flood};
 
 mod keep_alive;
-pub use keep_alive::{keep_alive_sets, HotPathTree};
+pub use keep_alive::{keep_alive_sets, HotPathTree, GuardFailure};
 
 mod allocator;
 pub use allocator::{Instruction, allocate};
@@ -50,25 +50,31 @@ impl<'a> Builder<'a> {
     }
 
     /**
-     * Converts `tree` into an [`EBB`]. Optimises the hot path in isolation,
-     * and recurses on the cold paths, passing information about [`Variable`]
-     * allocation and instruction scheduling.
+     * Converts a [`HotPathTree`] into an [`EBB`]. Optimises the hot path in
+     * isolation, and recurses on the cold paths, passing information about
+     * [`Variable`] allocation and instruction scheduling.
      *
      * On entry and on exit, `marks[node]` must be in `1..coldness` if
-     * `node` is before the guard from which `tree` diverges, and `0`
-     * otherwise. `marks[dataflow.entry_node()]` must be `1`.
+     * `node` is before the guard from which the `HotPathTree` diverges, and
+     * `0` otherwise. `marks[dataflow.entry_node()]` must be `1`.
      *
-     * - tree - the subtree to convert.
-     * - coldness - 2 + the number of cold branches needed to reach `tree`.
-     *   (`0` is used for unmarked nodes, and `1` for the entry node).
-     * - slots_used - the number of [`Slot`]s on entry to `tree`.
-     * - input - called once for each input to `tree`. It informs the caller
-     *   of `walk()` that the input is live, and returns the [`Variable`] that
-     *   holds it.
+     * - exit - the exit [`Node`] of the hot path.
+     * - leaf - the merge point of the hot path.
+     * - guard_failure - called for each [`Op::Guard`] to find out what to do
+     *   if the guard fails.
+     * - coldness - 2 + the number of cold branches needed to reach the
+     *   `HotPathTree`. (`0` is used for unmarked nodes, and `1` for
+     *   `dataflow.entry_node()`.
+     * - slots_used - the number of [`Slot`]s on entry to the `HotPathTree`.
+     * - input - called once for each input to the `HotPathTree`. It informs
+     *   the caller of `walk()` that the input is live, and returns the
+     *   [`Variable`] that holds it.
      */
-    pub fn walk<L: Debug + Clone>(
-        &mut self,
-        tree: &HotPathTree<L>,
+    pub fn walk<'w, L: Debug + Clone>(
+        &'w mut self,
+        exit: Node,
+        leaf: &'w L,
+        guard_failure: &dyn Fn(Node) -> &'w GuardFailure<L>,
         coldness: usize,
         slots_used: usize,
         input: &mut dyn FnMut(Out) -> Variable,
@@ -76,7 +82,7 @@ impl<'a> Builder<'a> {
     ) -> EBB<L> {
         let mut inputs = HashSet::new();
         let mut effects = HashSet::new();
-        let nodes = flood(self.dataflow, &mut self.marks, coldness, &mut inputs, &mut effects, tree.exit);
+        let nodes = flood(self.dataflow, &mut self.marks, coldness, &mut inputs, &mut effects, exit);
         let inputs: Box<[_]> = inputs.into_iter().collect(); // Define an order.
         let input_variables: Box<[_]> = inputs.iter().copied().map(input).collect();
         let mut variables: HashMap<Out, Variable> = inputs.iter().zip(&*input_variables).map(
@@ -88,8 +94,9 @@ impl<'a> Builder<'a> {
             &variables,
             self.dataflow,
             &*nodes,
-            |node| tree.children.get(&node).map(|child| &child.keep_alives),
+            |node| if self.is_guard(node) { Some(&guard_failure(node).keep_alives) } else { None },
         );
+
         // Allocate spill slots on the hot path.
         // Also, find the final location of each `Out`.
         let mut slots_used = before.slots_used;
@@ -115,13 +122,13 @@ impl<'a> Builder<'a> {
             allocation,
             slots_used,
             variables,
-            tree.exit,
-            lookup_leaf.after(&tree.leaf),
+            exit,
+            lookup_leaf.after(&leaf),
         );
 
         // Depth-first walk from the exits back to the entry.
-        let mut ending = Ending::Leaf(tree.leaf.clone());
-        self.marks[tree.exit] = 0;
+        let mut ending = Ending::Leaf(leaf.clone());
+        self.marks[exit] = 0;
         for &instruction in instructions.iter().rev() {
             match instruction {
                 Instruction::Spill(out1, out2) => {
@@ -129,15 +136,20 @@ impl<'a> Builder<'a> {
                 },
                 Instruction::Node(node) => {
                     if self.is_guard(node) {
+                        // Make an `EBB` for the hot path.
                         let hot = cg.ebb(ending);
-                        let gf = tree.children.get(&node).expect("Missing GuardFailure");
+                        // Make `EBB`s for the cold paths path.
+                        let gf = guard_failure(node);
                         let cold = gf.cold.map(|child| self.walk(
-                            child,
+                            child.exit,
+                            &child.leaf,
+                            &|guard_node| child.children.get(&node).unwrap_or_else(|| guard_failure(guard_node)),
                             coldness + 1,
                             cg.slots_used(),
                             &mut |out| cg.read(out),
                             lookup_leaf,
                         ));
+                        // Combine the hot and cold paths and update `ending`.
                         let cft_switch: cft::Switch<EBB<_>> = cold.insert_hot(hot);
                         ending = Ending::Switch(code::Switch::Index {
                             discriminant: {
@@ -179,7 +191,9 @@ pub fn build<L: Debug + Clone>(
     // Build the new `EBB`.
     let mut builder = Builder::new(dataflow);
     builder.walk(
-        &tree,
+        tree.exit,
+        &tree.leaf,
+        &|guard_node| tree.children.get(&guard_node).expect("Missing GuardFailure"),
         2,
         before.slots_used,
         &mut |out| *input_map.get(&out).unwrap(),
