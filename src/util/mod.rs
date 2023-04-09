576e13c67c44641c30772cdbdc6043784c24fdec
Alistair Turnbull <github@minworks.co.uk>
2023-04-09 05:05:24 +0100

    Write `util::{AdHocFn, Map, Usage}`.
    `Usage` generalise `optimizer::builder::allocator::Usage`.
    The other two were useful in the implementation of `Usage`.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,10 +3,13 @@ mod array_map;
 pub use array_map::{ArrayMap, AsUsize};
 
 mod iter;
-pub use iter::{map_filter_max};
+pub use iter::{map_filter_max, AdHocFn, First, Second, Map};
 
 mod comma_separated;
 pub use comma_separated::{CommaSeparated};
 
 mod rotate;
 pub use rotate::{rotate_left, rotate_right};
+
+mod usage;
+pub use usage::{Usage};
